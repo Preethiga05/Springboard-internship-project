3de6383ddec70bad744fd48605b03caf249ebd2d
Preethiga05 <162137817+Preethiga05@users.noreply.github.com>
2025-01-05 20:51:26 +0530

    Delete Backend directory

--- Backend/module2/src/main/java/com/infosys/module2/exception/Module2Exception.java
+++
@@ -1,8 +0,0 @@
-package com.infosys.module2.exception;
-
-public class Module2Exception extends Exception{
-
-    public Module2Exception(String message){
-        super(message);
-    }
-}
--- Backend/module3/src/main/java/com/infosys/module3/repository/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infosys.module3.repository;
-
-import com.infosys.module3.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
